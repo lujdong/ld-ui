c28610b619a7db00b6f843166299a9c1fe61d3c0
lujdong <zhouyuan8797@gmail.com>
2021-03-26 16:38:51 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-use-before-define': 0,
+    'react-hooks/rules-of-hooks': 2
+  }
+}
