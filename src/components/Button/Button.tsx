899f3974f534915156f685cfbfc7e2a8c9c8bce2
lujdong <zhouyuan8797@gmail.com>
2021-03-30 15:21:53 +0800

    feat: 添加按钮组件

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { CSSProperties, FC, MouseEventHandler } from 'react'
 import { tuple } from '../utils/type'
 import classnames from 'classnames'
 
@@ -11,25 +11,58 @@ export type ButtonType = typeof ButtonTypes[number]
 const ButtonHtmlTypes = tuple('button', 'submit', 'reset')
 export type ButtonHtmlType = typeof ButtonHtmlTypes[number]
 
-export interface ButtonProps {
+export interface NativeButtonProps {
+  htmlType?: ButtonHtmlType;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+export interface BaseButtonProps {
   /**
    * @description 大小
    */
   size?: SizeType;
+  /**
+   * @description 类型
+   */
+  type?: ButtonType;
+  /**
+   * @description 自定义类名
+   */
+  className?: string;
+  /**
+   * @description 自定义 style
+   */
+  style?: CSSProperties;
 }
 
-const Button: FC<ButtonProps> = ({ size }) => {
+export type ButtonProps = NativeButtonProps & BaseButtonProps
+
+const Button: FC<ButtonProps> = ({
+  size,
+  className,
+  children,
+  style,
+  type,
+  htmlType,
+  onClick
+}) => {
 
   const classes = classnames(
     'ld-btn',
+    className,
     {
-      [`ld-btn-${size}`]: size
+      [`ld-btn-${size}`]: size,
+      [`ld-btn-${type}`]: type,
     }
   )
 
   return (
-    <button className={classes}>
-      <span>button</span>
+    <button
+      className={classes}
+      style={style}
+      type={htmlType}
+      onClick={onClick}
+    >
+      <span>{children}</span>
     </button>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { MouseEventHandler } from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from './components'
 import './components/styles/index.less'
 
+const clickTest = () => {
+  console.log(123)
+}
 ReactDOM.render(
   <React.StrictMode>
-    <Button>hello</Button>
+    <div style={{
+      margin: '100px auto',
+      textAlign: 'center'
+    }}>
+      <Button onClick={clickTest}>默认按钮</Button>
+      <Button type="primary" onClick={clickTest}>主要按钮</Button>
+      <Button type="danger" onClick={clickTest}>危险按钮</Button>
+      <Button type="warning" onClick={clickTest}>警示按钮</Button>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 )
