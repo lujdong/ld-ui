586695fb12a27f4a4c0172ad7bd2c9e3cd01dcc3
lujdong <zhouyuan8797@gmail.com>
2021-03-30 14:23:21 +0800

    chore: 添加全局less变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,10 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       less: {
-        javascriptEnabled: true
+        javascriptEnabled: true,
+        modifyVars: {
+          hack: `true; @import (reference) "${resolve('src/components/styles/index.less')}";`
+        }
       }
     }
   }
